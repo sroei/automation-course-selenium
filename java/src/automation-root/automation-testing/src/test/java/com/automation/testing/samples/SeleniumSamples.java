a37144075a7ee943cee60d9c16a400f72f6b33f0
s_roe <s_roe@DESKTOP-6UUQQNF>
2019-05-21 21:22:55 +0300

    lecture 4.2

---
+++ java/src/automation-root/automation-testing/src/test/java/com/automation/testing/samples/SeleniumSamples.java
@@ -0,0 +1,11 @@
+package com.automation.testing.samples;
+
+import org.testng.annotations.Test;
+
+public class SeleniumSamples {
+
+    @Test
+    public void WebDriverSamples(){
+        // TODO: test logic here
+    }
+}
