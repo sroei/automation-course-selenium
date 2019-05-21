57870f21ca838cc8e73d319ed5b465ef3b81dbf2
s_roe <s_roe@DESKTOP-6UUQQNF>
2019-05-21 21:06:04 +0300

    lecture 4.1

---
+++ csharp/src/AutomationRoot/Automation.Testing/SeleniumSamples.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Testing
+{
+    [TestClass]
+    public class SeleniumSamples
+    {
+        [TestMethod]
+        public void WebDriverSamples()
+        {
+            // TODO: add test logic here
+        }
+    }
+}
