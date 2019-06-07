7cfd06630ec1013fcaab1567e5a0232ff8b11b64
s_roe <s_roe@DESKTOP-6UUQQNF>
2019-06-07 15:31:09 +0300

    lecture 5.14

---
+++ csharp/src/AutomationRoot/Automation.Extensions/Components/WebDriverExtensions.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Automation.Extensions.Components
+{
+    public static class WebDriverExtensions
+    {
+        public static IWebDriver GoToUrl(this IWebDriver driver, string url)
+        {
+            // actions
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+
+            // fluent
+            return driver;
+        }
+    }
+}
