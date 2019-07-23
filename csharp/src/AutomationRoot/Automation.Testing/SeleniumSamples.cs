06395367f8befa778ff2a72cdfdc57b37feec359
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-07-23 20:35:35 +0300

    lecture 8.5

--- csharp/src/AutomationRoot/Automation.Testing/SeleniumSamples.cs
+++ csharp/src/AutomationRoot/Automation.Testing/SeleniumSamples.cs
@@ -222,5 +222,15 @@ namespace Automation.Testing
             Thread.Sleep(2000);
             driver.Dispose();
         }
+
+        [TestMethod]
+        public void GoToUrlRemoteSample()
+        {
+            var driver = new WebDriverFactory(new DriverParams { Driver = "chrome", Binaries = "http://localhost:4444/wd/hub", Source = "remote" }).Get();
+
+            driver.GoToUrl("https://gravitymvctestapplication.azurewebsites.net/");
+            Thread.Sleep(2000);
+            driver.Dispose();
+        }
     }
 }
