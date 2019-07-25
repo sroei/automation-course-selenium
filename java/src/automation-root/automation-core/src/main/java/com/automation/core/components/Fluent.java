5b3aa890adeb74c70efa6b38e28b5c0e70505d5d
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-07-25 14:09:28 +0300

    lecture 9.16

--- java/src/automation-root/automation-core/src/main/java/com/automation/core/components/Fluent.java
+++ java/src/automation-root/automation-core/src/main/java/com/automation/core/components/Fluent.java
@@ -1,6 +1,9 @@
 package com.automation.core.components;
 
+import java.lang.reflect.InvocationTargetException;
+
 public interface Fluent {
-    <T> T changeContext();
-    <T> T changeContext(String application);
+    <T> T changeContext() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException;
+
+    <T> T changeContext(String application) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException;
 }
