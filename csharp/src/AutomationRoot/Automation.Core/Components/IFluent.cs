fd981efb9420bb12c6684e763a803e5164e3e22c
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-07-25 13:55:32 +0300

    lecture 9.15

--- csharp/src/AutomationRoot/Automation.Core/Components/IFluent.cs
+++ csharp/src/AutomationRoot/Automation.Core/Components/IFluent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Automation.Core.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace Automation.Core.Components
     public interface IFluent
     {
         T ChangeContext<T>();
+        T ChangeContext<T>(ILogger logger);
+        T ChangeContext<T>(string application, ILogger logger);
         T ChangeContext<T>(string application);
     }
 }
