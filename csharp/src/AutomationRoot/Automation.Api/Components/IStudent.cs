fdce0efb4b261b9350450007d4d952af05d9e8fe
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-07-26 12:37:08 +0300

    lecture 9.23

---
+++ csharp/src/AutomationRoot/Automation.Api/Components/IStudent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Api.Components
+{
+    public interface IStudent
+    {
+        string FirstName { get; }
+        string LastName { get; }
+        DateTime EnrollmentDate { get; }
+    }
+}
