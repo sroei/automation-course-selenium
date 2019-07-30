56639dde85f29eb215e7a0e511e94813bb2a93b0
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-07-30 21:00:26 +0300

    lecture 9.27

---
+++ csharp/src/AutomationRoot/Automation.Api/Components/IPageNavigator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Api.Components
+{
+    public interface IPageNavigator<out T>
+    {
+        T Next();
+        T Previous();
+        int Pages();
+        int Page();
+    }
+}
