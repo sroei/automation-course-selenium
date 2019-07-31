a31d478fa3ef69e619af4fbc3a0b94843cab00f8
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-07-31 17:39:36 +0300

    lecture 9.35

--- csharp/src/AutomationRoot/Automation.Api/Pages/IStudents.cs
+++ csharp/src/AutomationRoot/Automation.Api/Pages/IStudents.cs
@@ -1,10 +1,11 @@
 ﻿using Automation.Api.Components;
+using Automation.Core.Components;
 using System;
 using System.Collections.Generic;
 
 namespace Automation.Api.Pages
 {
-    public interface IStudents : IPageNavigator<IStudents>, IMenu, ICreate<ICreateStudent>
+    public interface IStudents : IFluent, IPageNavigator<IStudents>, IMenu, ICreate<ICreateStudent>
     {
         IStudents FindByName(string name);
         IEnumerable<IStudent> Students();
---
+++ csharp/src/AutomationRoot/Automation.Framework.Ui/Pages/StudentsUi.cs
@@ -0,0 +1,61 @@
+﻿using Automation.Api.Components;
+using Automation.Api.Pages;
+using Automation.Core.Components;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Framework.Ui.Pages
+{
+    public class StudentsUi : FluentUi, IStudents
+    {
+        public StudentsUi(IWebDriver driver)
+            : base(driver) { }
+
+        public StudentsUi(IWebDriver driver, Core.Logging.ILogger logger)
+            : base(driver, logger) { }
+
+        public ICreateStudent Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStudents FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Menu<T>(string menuName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStudents Next()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Page()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Pages()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IStudents Previous()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IStudent> Students()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
