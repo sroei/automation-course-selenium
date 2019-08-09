871002fef4a0828cda56b71c02c851e506f31f3e
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-08-09 11:46:58 +0300

    lecture 11.3

--- csharp/src/AutomationRoot/Automation.Core/Components/FluentUi.cs
+++ csharp/src/AutomationRoot/Automation.Core/Components/FluentUi.cs
@@ -4,47 +4,33 @@ using System;
 
 namespace Automation.Core.Components
 {
-    public class FluentUi : IFluent
+    public class FluentUi : FluentBase
     {
         public FluentUi(IWebDriver driver)
             : this(driver, new TraceLogger()) { }
 
-        public FluentUi(IWebDriver driver, ILogger logger)
+        public FluentUi(IWebDriver driver, ILogger logger) : base(logger)
         {
             Driver = driver;
-            Logger = logger;
         }
 
         public IWebDriver Driver { get; }
-        public ILogger Logger { get; }
 
-        public T ChangeContext<T>()
-        {
-            var instance = Create<T>(null);
-            Logger.Debug($"instance of [{GetType()?.FullName}] created");
-            return instance;
-        }
-
-        public T ChangeContext<T>(ILogger logger)
-        {
-            return Create<T>(logger);
-        }
-
-        public T ChangeContext<T>(string application, ILogger logger)
+        public override T ChangeContext<T>(string application, ILogger logger)
         {
             Driver.Navigate().GoToUrl(application);
             Driver.Manage().Window.Maximize();
             return Create<T>(logger);
         }
 
-        public T ChangeContext<T>(string application)
+        public override T ChangeContext<T>(string application)
         {
             Driver.Navigate().GoToUrl(application);
             Driver.Manage().Window.Maximize();
             return Create<T>(null);
         }
 
-        private T Create<T>(ILogger logger)
+        internal override T Create<T>(ILogger logger)
         {
             return logger == null
                 ? (T)Activator.CreateInstance(typeof(T), new object[] { Driver })
