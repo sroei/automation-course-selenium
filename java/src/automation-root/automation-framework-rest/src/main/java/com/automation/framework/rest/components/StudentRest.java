2f80147144eb2a41bfb8621c2a8febb56f37cc1d
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-08-11 21:41:51 +0300

    lecture 11.12

---
+++ java/src/automation-root/automation-framework-rest/src/main/java/com/automation/framework/rest/components/StudentRest.java
@@ -0,0 +1,48 @@
+package com.automation.framework.rest.components;
+
+import com.automation.api.components.Student;
+import com.automation.core.components.FluentRest;
+import com.automation.core.logging.Logger;
+import okhttp3.OkHttpClient;
+
+import java.time.LocalDateTime;
+
+public class StudentRest extends FluentRest implements Student {
+    public StudentRest(OkHttpClient httpClient) {
+        super(httpClient);
+    }
+
+    public StudentRest(OkHttpClient httpClient, Logger logger) {
+        super(httpClient, logger);
+    }
+
+    @Override
+    public Object edit() {
+        return null;
+    }
+
+    @Override
+    public Object details() {
+        return null;
+    }
+
+    @Override
+    public Object delete() {
+        return null;
+    }
+
+    @Override
+    public String firstName() {
+        return null;
+    }
+
+    @Override
+    public String lastName() {
+        return null;
+    }
+
+    @Override
+    public LocalDateTime enrollmentDate() {
+        return null;
+    }
+}
