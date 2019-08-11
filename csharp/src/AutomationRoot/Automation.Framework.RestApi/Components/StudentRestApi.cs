0d7a1ca8c0f9af084fc15036a2ef06f68e83ea29
s_roe <s_roe@DESKTOP-EJ38PCS>
2019-08-11 21:36:00 +0300

    lecture 11.11

---
+++ csharp/src/AutomationRoot/Automation.Framework.RestApi/Components/StudentRestApi.cs
@@ -0,0 +1,51 @@
+﻿using Automation.Api.Components;
+using Automation.Core.Components;
+using Automation.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Framework.RestApi.Components
+{
+    public class StudentRestApi : FluentRestApi, IStudent
+    {
+        public StudentRestApi(HttpClient httpClient)
+            : base(httpClient) { }
+
+        public StudentRestApi(HttpClient httpClient, ILogger logger)
+            : base(httpClient, logger) { }
+
+        public object Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Details()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DateTime EnrollementDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FirstName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LastName()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
