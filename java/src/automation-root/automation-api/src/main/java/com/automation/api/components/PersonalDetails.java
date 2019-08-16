fedbadd087c5614a4acfd310af0f2ca7f0b1ec7f
s_roe <s_roei@msn.com>
2019-08-16 13:55:13 +0300

    lecture 11.39

--- java/src/automation-root/automation-api/src/main/java/com/automation/api/components/PersonalDetails.java
+++ java/src/automation-root/automation-api/src/main/java/com/automation/api/components/PersonalDetails.java
@@ -1,9 +1,10 @@
 package com.automation.api.components;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public interface PersonalDetails {
-    String firstName();
+    String firstName() throws IOException;
 
     String lastName();
 
--- java/src/automation-root/automation-framework-rest/src/main/java/com/automation/framework/rest/pages/StudentDetailsRest.java
+++ java/src/automation-root/automation-framework-rest/src/main/java/com/automation/framework/rest/pages/StudentDetailsRest.java
@@ -1,27 +1,41 @@
 package com.automation.framework.rest.pages;
 
 import com.automation.api.components.Enrollment;
+import com.automation.api.components.Student;
 import com.automation.api.pages.StudentDetails;
 import com.automation.core.components.FluentRest;
 import com.automation.core.logging.Logger;
 import com.automation.core.logging.TraceLogger;
+import com.automation.framework.rest.components.StudentRest;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class StudentDetailsRest extends FluentRest implements StudentDetails {
     private int studentId;
+    private String fName;
 
-    public StudentDetailsRest(OkHttpClient httpClient) {
+    public StudentDetailsRest(OkHttpClient httpClient) throws IOException {
         this(httpClient, new TraceLogger());
     }
 
-    private StudentDetailsRest(OkHttpClient httpClient, Logger logger) {
-        this(httpClient, logger, "http://localhost");
+    private StudentDetailsRest(OkHttpClient httpClient, Logger logger) throws IOException {
+        this(httpClient, logger, "http://localhost",0);
     }
 
-    private StudentDetailsRest(OkHttpClient httpClient, Logger logger, String baseUrl) {
+    public StudentDetailsRest(OkHttpClient httpClient, Logger logger, String baseUrl, int studentId) throws IOException {
         super(httpClient, logger, baseUrl);
+        this.studentId = studentId;
+        build();
     }
 
     @Override
@@ -31,7 +45,7 @@ public class StudentDetailsRest extends FluentRest implements StudentDetails {
 
     @Override
     public String firstName() {
-        return null;
+        return fName;
     }
 
     @Override
@@ -45,8 +59,22 @@ public class StudentDetailsRest extends FluentRest implements StudentDetails {
     }
 
     // non interface fields
-    public StudentDetailsRest setStudentId(int studentId) {
+    public StudentDetailsRest setStudentId(int studentId) throws IOException {
         this.studentId = studentId;
-        return this;
+        return new StudentDetailsRest(getHttpClient(), getLogger(), getBaseUrl(), studentId);
+    }
+
+    // build pipeline
+    private void build() throws IOException {
+        // compose route
+        String route = String.format("/api/Students/%d", studentId);
+
+        // get all data-rows
+        Request request = new Request.Builder().url(getBaseUrl() + "/api/Students").get().build();
+        String responseBody = Objects.requireNonNull(getHttpClient().newCall(request).execute().body()).string();
+        JsonObject dataRow = new JsonParser().parse(responseBody).getAsJsonObject();
+
+        // set data
+        fName = dataRow.get("firstMidName").getAsString();
     }
 }
