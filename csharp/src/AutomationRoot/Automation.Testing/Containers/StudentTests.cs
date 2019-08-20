a74fae0e2a32740d84716f82e7dde170af312cf2
s_roe <s_roei@msn.com>
2019-08-20 21:01:15 +0300

    lecture 11.50

--- csharp/src/AutomationRoot/Automation.Testing/Containers/StudentTests.cs
+++ csharp/src/AutomationRoot/Automation.Testing/Containers/StudentTests.cs
@@ -13,15 +13,15 @@ namespace Automation.Testing.Containers
     public class StudentTests
     {
         [DataTestMethod]
-        //[DataRow("" +
-        //    "{" +
-        //    "'driver':'CHROME'," +
-        //    "'keyword':" +
-        //    "'Alexander'," +
-        //    "'application':'https://gravitymvctestapplication.azurewebsites.net/Student'," +
-        //    "'fluent':'Automation.Core.Components.FluentUi'," +
-        //    "'students':'Automation.Framework.Ui.Pages.StudentsUi'" +
-        //    "}")]
+        [DataRow("" +
+            "{" +
+            "'driver':'CHROME'," +
+            "'keyword':" +
+            "'Alexander'," +
+            "'application':'https://gravitymvctestapplication.azurewebsites.net/Student'," +
+            "'fluent':'Automation.Core.Components.FluentUi'," +
+            "'students':'Automation.Framework.Ui.Pages.StudentsUi'" +
+            "}")]
         [DataRow("" +
             "{" +
             "'driver':'HTTP'," +
