b5d6744e45dda2d5f007e3e2bf98622471350dbc
s_roe <s_roei@msn.com>
2019-08-20 21:21:44 +0300

    lecture 11.52

--- csharp/src/AutomationRoot/Automation.Framework.RestApi/Pages/StudentDetailsRest.cs
+++ csharp/src/AutomationRoot/Automation.Framework.RestApi/Pages/StudentDetailsRest.cs
@@ -1,6 +1,9 @@
 ﻿using Automation.Api.Components;
 using Automation.Api.Pages;
 using Automation.Core.Components;
+using Automation.Core.Logging;
+using Automation.Framework.RestApi.Components;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +16,16 @@ namespace Automation.Framework.RestApi.Pages
     public class StudentDetailsRest : FluentRest, IStudentDetails
     {
         public StudentDetailsRest(HttpClient httpClient)
-            : base(httpClient) { }
+            : this(httpClient, new TraceLogger()) { }
 
-        public StudentDetailsRest(HttpClient httpClient, Core.Logging.ILogger logger)
-            : base(httpClient, logger) { }
+        public StudentDetailsRest(HttpClient httpClient, ILogger logger)
+            : this(httpClient, logger, 0) { }
+
+        public StudentDetailsRest(HttpClient httpClient, ILogger logger, int id)
+            : base(httpClient, logger)
+        {
+            Build(id);
+        }
 
         public DateTime EnrollementDate()
         {
@@ -36,6 +45,17 @@ namespace Automation.Framework.RestApi.Pages
         public string LastName()
         {
             throw new NotImplementedException();
+        }
+
+        // build pipeline
+        private void Build(int id)
+        {
+            var response = HttpClient.GetAsync($"/api/Students/{id}").GetAwaiter().GetResult();
+            if (!response.IsSuccessStatusCode)
+            {
+                return;
+            }
+            var responseBody = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
         }
     }
 }
